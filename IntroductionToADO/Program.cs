a66f1a77f19c0ae18fd69c473e57badc93c52f55
Koros_zl65@top-academy.ru <Koros_zl65@top-academy.ru>
2024-02-09 01:45:00 +0500

    Add project files.

---
+++ IntroductionToADO/Program.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+
+namespace IntroductionToADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connection_string = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LiberyPD212;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection connection = new SqlConnection(connection_string); // создаем соединения с сервером при создании оно не активно
+            connection.Open(); // открываем соединения, поскольку оно не автоматическое, и не открывает
+            // вставка 
+            //string insert_author = @"INSERT INTO Authors (first_name,last_name) VALUES ('Leo','Tolstoy')"; // запрос в SQL
+            string insert_string = @"INSERT INTO Books (author,title, price, pages) VALUES ('5','War and Peace',240, 2000)"; // запрос в SQL
+            //чтобы выполнить запрос нужен команда
+            //SqlCommand cmd = new SqlCommand(insert_author, connection);
+            SqlCommand cmd = new SqlCommand(insert_string, connection);
+            //cmd.ExecuteNonQuery();  // - этот метод для команд Insert, Upgate, Delete
+
+            // выборка 
+            string select_string = @"Select * From Authors";
+            cmd.CommandText = select_string;
+            
+            SqlDataReader rdl =  cmd.ExecuteReader();
+            while (rdl.Read())
+            {
+                Console.WriteLine($"{rdl[0]} {rdl[1]} {rdl[2]}");
+            }
+
+
+            connection.Close(); // соединения нужно закрывать 
+        }
+    }
+}
